89f9b9aa69d9b488c63c3036ef94e481d6dddc85
FathimaShafana <77454499+FathimaShafana@users.noreply.github.com>
2021-03-05 21:50:44 +0530

    Add files via upload
    Apply and approve doesn't seems to be working.

---
+++ SpringBoot_Hibernate/src/main/java/com/example/demo/SpringBootHibernateApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootHibernateApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootHibernateApplication.class, args);
+	}
+
+}
