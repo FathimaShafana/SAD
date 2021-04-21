e4b3e94353ad1c3d6703d9045fe8139c8ddd2f17
FathimaShafana <77454499+FathimaShafana@users.noreply.github.com>
2021-04-21 23:55:07 +0530

    Session using Redis & JDBC

---
+++ Spring_Session/SpringSession_JDBC/target/classes/data.sql
@@ -0,0 +1,2 @@
+insert into user (username, password, active, role, email) values ('Admin', '$2a$10$otMn8Xre2SYZm.cZwa4q8e7hq3Y7/gkGoD9FOMvVmRvPr75p3POLm', true, 'ROLE_ADMIN', 'admin@ait.asia');
+insert into user (username, password,  active, role, email) values ('david', '$2a$10$otMn8Xre2SYZm.cZwa4q8e7hq3Y7/gkGoD9FOMvVmRvPr75p3POLm', true, 'ROLE_USER', 'john@ait.asia');
